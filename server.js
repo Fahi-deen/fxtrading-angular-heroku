1318d60d2c654bd646c4736482059967f33bdcd4
Fahi-deen <fahideen786@gmail.com>
2022-09-16 19:57:53 +0530

    test1

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/FXTRADINGANGULAR'));
+app.use(express.static('./dist/fx-trading-angular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/fx-trading-angular'}),
 );
 
 // Start the app by listening on the default Heroku port
